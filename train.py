1d99774a82f8402ec4ccc78c34b61806f286c7f1
S-Choudhuri <schoud13@asu.edu>
2019-09-19 00:21:39 -0700

    First commit

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from PIL import Image
 from keras.utils import to_categorical
 from keras.layers import Input
 from keras.optimizers import Adam
+from sklearn.metrics import accuracy_score
 import model
 import loss
 
@@ -71,50 +72,67 @@ def train(param):
     models["combined_model"] = model.build_combined_model(inp, [classifier, discriminator])
     models["combined_model"].compile(optimizer="Adam",loss={'c_dense2': 'categorical_crossentropy', 'd_dense2': \
                        'binary_crossentropy'}, loss_weights={'c_dense2': 1, 'd_dense2': 2}, metrics=['accuracy'])
-    
+
+    Xs, ys = param["source_data"], param["source_label"]
+    Xt, yt = param["target_data"], param["target_label"]
+
     y_adversarial_1 = np.array(([1] * param["batch_size"] + [0] * param["batch_size"]))
+    sample_weights_class = np.array(([1] * param["batch_size"] + [0] * param["batch_size"]))
     sample_weights_adversarial = np.ones((param["batch_size"] * 2,))
-    S_batches = batch_generator([Xs, to_categorical(ys)], param["batch_size"])
-    T_batches = batch_generator([Xt, np.zeros(shape = (len(Xt),2))], param["batch_size"])
+    S_batches = batch_generator([Xs, ys], param["batch_size"])
+    T_batches = batch_generator([Xt, np.zeros(shape = (len(Xt),))], param["batch_size"])
+
+    param["target_accuracy"] = 0
 
     for i in range(param["num_iterations"]):
-        y_adversarial_2 = to_categorical(np.array(([0] * batch_size + [1] * batch_size)))
+        y_adversarial_2 = np.array(([0] * param["batch_size"] + [1] * param["batch_size"]))
         X0, y0 = next(S_batches)
         X1, y1 = next(T_batches)
         X_adv = np.concatenate([X0, X1])
         y_class = np.concatenate([y0, np.zeros_like(y0)])
         adv_weights = []
-        for layer in model.layers:
-            if (layer.name.startswith("do")):
+        for layer in models["combined_model"].layers:
+            if (layer.name.startswith("d_dense2")):
                 adv_weights.append(layer.get_weights())
-        we = domain_classification_model.predict(X0)
-            we = 1-we[:,0]
-            we = we.tolist()
-            sample_weights_class = np.array((we + [0] * batch_size))            
-            stats = model.train_on_batch(X_adv, [y_class, y_adversarial_1],
-                                     sample_weight=[sample_weights_class, sample_weights_adversarial])            
-            k = 0
-            for layer in model.layers:
-                if (layer.name.startswith("do")):                    
-                    layer.set_weights(adv_weights[k])
-                    k += 1
-            class_weights = []        
-            for layer in model.layers:
-                if (not layer.name.startswith("do")):
-                    class_weights.append(layer.get_weights())            
-            stats2 = domain_classification_model.train_on_batch(X_adv, [y_adversarial_2])
-            k = 0
-            for layer in model.layers:
-                if (not layer.name.startswith("do")):
-                    layer.set_weights(class_weights[k])
-                    k += 1
+          
+        stats = models["combined_model"].train_on_batch(X_adv, [y_class, y_adversarial_1],\
+                                sample_weight=[sample_weights_class, sample_weights_adversarial])            
+        k = 0
+        for layer in models["combined_model"].layers:
+            if (layer.name.startswith("d_dense2")):                    
+                layer.set_weights(adv_weights[k])
+                k += 1
 
-        if ((i + 1) % 1000 == 0):
-            # print(i, stats)
-            y_test_hat_t = source_classification_model.predict(Xt).argmax(1)
-            y_test_hat_s = source_classification_model.predict(Xs).argmax(1)
-            print("Iteration %d, source accuracy =  %.3f, target accuracy = %.3f"%(i, accuracy_score(ys, y_test_hat_s), accuracy_score(yt, y_test_hat_t)))
+        class_weights = []        
+        for layer in models["combined_model"].layers:
+            if (not layer.name.startswith("d_dense2")):
+                class_weights.append(layer.get_weights())  
 
+        stats2 = models["combined_discriminator"].train_on_batch(X_adv, [y_adversarial_2])
+
+        k = 0
+        for layer in models["combined_model"].layers:
+            if (not layer.name.startswith("d_dense2")):
+                layer.set_weights(class_weights[k])
+                k += 1
+
+        if ((i + 1) % param["test_interval"] == 0):
+            y_test_hat_t = models["combined_classifier"].predict(Xt)
+            y_test_hat_s = models["combined_classifier"].predict(Xs)
+
+            print(ys.shape, y_test_hat_s.shape)
+            source_accuracy = accuracy_score(ys, y_test_hat_s)
+            target_accuracy = accuracy_score(yt, y_test_hat_t)
+            log_str = "iter: {:05d}, source_accuracy: {:.5f}, target_accuracy: {:.5f}".format(i, source_accuracy*100, target_accuracy*100)
+            print(log_str)
+            if param["target_accuracy"] < target_accuracy:              
+                #model["optimal"] = models["combined_model"]
+                param["output_file"].write(log_str)
+                param["output_file"].flush()
+
+        #if i % param["snapshot_interval"] == 0:
+            #model["optimal"].save(os.path.join(param["output_path"],"iter_{:05d}_model.h5".format(i)))
+            #model["optimal"].save(os.path.join(param["output_path"],"iter_model.h5"))
 
 if __name__ == "__main__":
     # Read parameter values from the console
@@ -124,9 +142,9 @@ if __name__ == "__main__":
     parser.add_argument('--dataset_name', type=str, default='office', help="Name of the source dataset")
     parser.add_argument('--source_path', type=str, default='Data/Office/amazon_10_list.txt', help="Path to source dataset")
     parser.add_argument('--target_path', type=str, default='Data/Office/webcam_10_list.txt', help="Path to target dataset")
-    parser.add_argument('--batch_size', type=int, default=16, help="Batch size for training")
-    parser.add_argument('--test_interval', type=int, default=500, help="Gap between two successive test phases")
-    parser.add_argument('--snapshot_interval', type=int, default=5000, help="Gap between saving output models")
+    parser.add_argument('--batch_size', type=int, default=32, help="Batch size for training")
+    parser.add_argument('--test_interval', type=int, default=50, help="Gap between two successive test phases")
+    parser.add_argument('--snapshot_interval', type=int, default=100, help="Gap between saving output models")
     parser.add_argument('--output_dir', type=str, default='models', help="Directory for saving output model")
     args = parser.parse_args()
 
@@ -136,7 +154,7 @@ if __name__ == "__main__":
     # Initialize parameters
     param = {}
     param["inp_dims"] = [224, 224, 3]
-    param["num_iterations"] = 12004
+    param["num_iterations"] = 200
     param["batch_size"] = args.batch_size
     param["test_interval"] = args.test_interval
     param["source_path"] = args.source_path
