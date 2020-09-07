7cfc5456e3380e60044287f70564f7c910da0737
Sandipan Choudhuri <schoud13@asu.edu>
2020-09-07 13:45:45 -0700

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -1,6 +1,7 @@
 import numpy as np
-from keras.optimizers import Adam
+from keras.optimizers import Adam, SGD
 
+'''
 def opt_classifier(param):
     return Adam(lr=param["lr_classifier"], beta_1=param["b1_classifier"], beta_2=param["b2_classifier"])
 
@@ -9,4 +10,12 @@ def opt_discriminator(param):
 
 def opt_combined(param):
     return Adam(lr=param["lr_combined"], beta_1=param["b1_combined"], beta_2=param["b2_combined"])
+'''
+def opt_classifier(param):
+    return SGD(lr=param["lr_classifier"], momentum = 0.9, nesterov=True)
 
+def opt_discriminator(param):
+    return SGD(lr=param["lr_discriminator"], momentum = 0.9, nesterov=True)
+
+def opt_combined(param):
+    return SGD(lr=param["lr_combined"], momentum = 0.9, nesterov=True)
